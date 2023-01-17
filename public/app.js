6de416a3b27a04d4856fcf51f60cec0eb8255a3a
Todd Pangilinan <pang.todd@gmail.com>
2023-01-17 14:47:39 -0800

    added invoice class to app.ts.

--- public/app.js
+++ public/app.js
@@ -1,11 +1,23 @@
 "use strict";
-// const anchor = document.querySelector('a');
-// // if(anchor){
-// // console.log(anchor.href);
-// // }
-// console.log(anchor.href);
-// const form = document.querySelector('form');
+// classes
+class Invoice {
+    constructor(c, d, a) {
+        this.client = c;
+        this.details = d;
+        this.amount = a;
+    }
+    format() {
+        return `${this.client} owes $${this.amount} for ${details}`;
+    }
+}
+const invOne = new Invoice("mario", "work on Marioworld", 400);
+const invTwo = new Invoice("Luigi", "Luigi.com work", 250);
+let invoices = [];
+invoices.push(invOne);
+invoices.push(invTwo);
+console.log(invoices);
 const form = document.querySelector(".new-item-form");
+//inputs
 const type = document.querySelector('#type');
 const toFrom = document.querySelector('#toFrom');
 const details = document.querySelector('#details');
--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,32 @@
-// const anchor = document.querySelector('a');
+// classes
+class Invoice{
+  client: string;
+  details: string;
+  amount: number;
 
-// // if(anchor){
-// // console.log(anchor.href);
-// // }
+  constructor(c: string, d: string, a: number){
+    this.client = c;
+    this.details = d;
+    this.amount = a;
+  }
 
-// console.log(anchor.href);
+  format(){
+    return `${this.client} owes $${this.amount} for ${details}`
+  }
+}
 
-// const form = document.querySelector('form');
+const invOne = new Invoice("mario", "work on Marioworld", 400);
+const invTwo = new Invoice("Luigi", "Luigi.com work", 250)
+
+let invoices: Invoice[]= [];
+invoices.push(invOne);
+invoices.push(invTwo);
+
+console.log(invoices)
 
 const form = document.querySelector(".new-item-form") as HTMLFormElement;
 
+//inputs
 const type = document.querySelector('#type') as HTMLSelectElement;
 const toFrom = document.querySelector('#toFrom') as HTMLInputElement;
 const details = document.querySelector('#details') as HTMLInputElement;
@@ -22,6 +39,5 @@ form.addEventListener('submit', (e: Event)=> {
     toFrom.value,
     details.value,
     amount.valueAsNumber
-  )
-}
-)
+  );
+});
