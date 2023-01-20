b966bd654ce6da185aacf1bcb18c5544b7d059ed
Todd Pangilinan <pang.todd@gmail.com>
2023-01-19 22:51:09 -0800

    set up import for HasFormatter, and set up a conditional to take value as Invoice or Payment.

--- public/app.js
+++ public/app.js
@@ -1,12 +1,20 @@
-import { Invoice } from './classes/invoice.js';
-const invOne = new Invoice("mario", "work on Marioworld", 400);
-const invTwo = new Invoice("Luigi", "Luigi.com work", 250);
-let invoices = [];
-invoices.push(invOne);
-invoices.push(invTwo);
-invoices.forEach(inv => {
-    console.log(inv.client, inv.amount, inv.format());
-});
+import { Invoice } from './classes/Invoice.js';
+import { Payment } from './classes/Payment.js';
+// let docOne: HasFormatter;
+// let docTwo: HasFormatter;
+// docs.push(docTwo);docOne = new Invoice("yoshi", "webwork", 240);
+// docTwo = new Payment("Mario", 'plumbing work', 100);
+// let docs: HasFormatter[]=[];
+// docs.push(docOne);
+// console.log(docs);
+// const invOne = new Invoice("mario", "work on Marioworld", 400);
+// const invTwo = new Invoice("Luigi", "Luigi.com work", 250)
+// let invoices: Invoice[]= [];
+// invoices.push(invOne);
+// invoices.push(invTwo);
+// invoices.forEach(inv=>{
+//   console.log(inv.client, inv.amount, inv.format());
+// });
 const form = document.querySelector(".new-item-form");
 //inputs
 const type = document.querySelector('#type');
@@ -15,5 +23,12 @@ const details = document.querySelector('#details');
 const amount = document.querySelector('#amount');
 form.addEventListener('submit', (e) => {
     e.preventDefault();
-    console.log(type.value, toFrom.value, details.value, amount.valueAsNumber);
+    let doc;
+    if (type.value === 'invoice') {
+        doc = new Invoice(toFrom.value, details.value, amount.valueAsNumber);
+    }
+    else {
+        doc = new Payment(toFrom.value, details.value, amount.valueAsNumber);
+    }
+    console.log(doc);
 });
--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,30 @@
-import { Invoice } from './classes/invoice.js';
+import { Invoice } from './classes/Invoice.js';
+import { Payment } from './classes/Payment.js';
+import { HasFormatter } from './interfaces/HasFormatter.js';
 
-const invOne = new Invoice("mario", "work on Marioworld", 400);
-const invTwo = new Invoice("Luigi", "Luigi.com work", 250)
+// let docOne: HasFormatter;
+// let docTwo: HasFormatter;
 
-let invoices: Invoice[]= [];
-invoices.push(invOne);
-invoices.push(invTwo);
 
-invoices.forEach(inv=>{
-  console.log(inv.client, inv.amount, inv.format());
-});
+// docs.push(docTwo);docOne = new Invoice("yoshi", "webwork", 240);
+// docTwo = new Payment("Mario", 'plumbing work', 100);
+
+// let docs: HasFormatter[]=[];
+
+// docs.push(docOne);
+
+// console.log(docs);
+
+// const invOne = new Invoice("mario", "work on Marioworld", 400);
+// const invTwo = new Invoice("Luigi", "Luigi.com work", 250)
+
+// let invoices: Invoice[]= [];
+// invoices.push(invOne);
+// invoices.push(invTwo);
+
+// invoices.forEach(inv=>{
+//   console.log(inv.client, inv.amount, inv.format());
+// });
 
 const form = document.querySelector(".new-item-form") as HTMLFormElement;
 
@@ -21,10 +36,12 @@ const amount = document.querySelector('#amount') as HTMLInputElement;
 
 form.addEventListener('submit', (e: Event)=> {
   e.preventDefault();
-  console.log(
-    type.value,
-    toFrom.value,
-    details.value,
-    amount.valueAsNumber
-  );
+
+  let doc: HasFormatter;
+  if (type.value === 'invoice'){
+    doc = new Invoice(toFrom.value, details.value, amount.valueAsNumber)
+  } else {
+    doc = new Payment(toFrom.value, details.value, amount.valueAsNumber)
+  }
+  console.log(doc);
 });
